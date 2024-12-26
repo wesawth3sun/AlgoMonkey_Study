a32a454ad185f8d71d975e0ab4356d2a6cb99ab2
SUN <suunn001@gmail.com>
2024-12-26 22:41:51 +0900

    완주하지 못한 선수

---
+++ wesawth3sun_java/Test2.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String[] participant = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion = {"josipa", "filipa", "marina", "nikola"};
+
+        HashMap<String, Integer> map = new HashMap<>();
+        String answer = "";
+
+        for (String string : participant) {
+            map.put(string, map.getOrDefault(string, 0) + 1);
+            //key가 존재하면 해당 key에 매핑되어 있는 값, 그렇지 않으면 디폴트 값을 반환
+        }
+
+        for (String string : completion) {
+            map.put(string, map.getOrDefault(string, 0) - 1);
+        }
+
+        for (String string : map.keySet()) {
+            if (map.get(string) == 1) {
+                answer = string;
+            }
+        }
+        System.out.println(answer);
+        System.out.println(map);
+    }
+}
